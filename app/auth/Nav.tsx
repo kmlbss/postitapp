637b4ef1a9008175de81779a8fe330b83f7af29c
kmlbss <49821804+kmlbss@users.noreply.github.com>
2023-03-14 01:31:12 +0400

    logged in user info displaying

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import Login from "./Login";
+import Logged from './Logged';
 import { getServerSession } from "next-auth";
 import { authOptions } from '../../pages/api/auth/[...nextauth]';
 
 export default async function Nav() {
   const session = await getServerSession(authOptions);
-  console.log("session", session);
   return (
     <nav className="flex justify-between items-center py-8 ">
       <Link href={"/"}>
@@ -14,7 +14,8 @@ export default async function Nav() {
       </Link>
 
       <ul className="flex items-center gap-6">
-        <Login />
+        { !session?.user && <Login />}
+        { session?.user && <Logged image={session.user?.image || ''} />}
       </ul>
     </nav>
   );
